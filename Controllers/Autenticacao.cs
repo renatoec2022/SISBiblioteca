5b571d6b5362889a3fa4b392b05e52f784e0779d
renatoec2022 <renato.engenheiro.computacao@gmail.com>
2022-02-07 22:16:07 -0300

    Implementada melhoria para gstão do acesso por usuarios cadastrados

--- Controllers/Autenticacao.cs
+++ Controllers/Autenticacao.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using Biblioteca.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Biblioteca.Controllers
 {
@@ -11,6 +13,54 @@ namespace Biblioteca.Controllers
             if(string.IsNullOrEmpty(controller.HttpContext.Session.GetString("user")))
             {
                 controller.Request.HttpContext.Response.Redirect("/Home/Login");
+            }
+        }
+        public static bool veirifcaLoginSenha(string login, string senha, Controller controller)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                
+                veirifcaSeUsuarioAdminExiste(bc);
+                
+                senha = Criptografo.TextoCriptografado(senha);
+
+                IQueryable<Usuario> UsuarioEncontrado = bc.usuarios.Where(u => u.login==login && u.senha == senha);
+                List<Usuario> ListaUsuarioEncontrado = UsuarioEncontrado.ToList();
+                if(ListaUsuarioEncontrado.Count==0)
+                {
+                    return false;
+                }
+                else
+                {
+                    controller.HttpContext.Session.SetString("login", ListaUsuarioEncontrado[0].login);
+                    controller.HttpContext.Session.SetString("Nome", ListaUsuarioEncontrado[0].Nome);
+                    controller.HttpContext.Session.SetInt32("tipo", ListaUsuarioEncontrado[0].tipo);
+                    return true;
+                }
+
+            }
+        }
+        public static void veirifcaSeUsuarioAdminExiste(BibliotecaContext bc)
+        {
+            IQueryable<Usuario> userEncontrado = bc.usuarios.Where(u => u.login == "admin");
+            //Não existe Usuário Admin: Criar o usuario adminn padrão
+            if (userEncontrado.ToList().Count == 0)
+            {
+                Usuario admin = new Usuario();
+                admin.login = "admin";
+                admin.senha = Criptografo.TextoCriptografado("123");
+                admin.tipo = Usuario.ADMIN;
+                admin.Nome = "Administração";
+
+                bc.usuarios.Add(admin);
+                bc.SaveChanges();
+            }
+        }
+        public static void veirifcaSeUsuarioEAdmin(Controller controller)
+        { 
+            if(!(controller.HttpContext.Session.GetInt32("tipo")==Usuario.ADMIN))
+            {
+                controller.Request.HttpContext.Response.Redirect("/Usuarios/NeedAdmin");
             }
         }
     }
