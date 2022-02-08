ae9ee75cc5b6680fdc579cce130f7e889dea0234
renatoec2022 <renato.engenheiro.computacao@gmail.com>
2022-02-07 22:37:08 -0300

    Tarefa de correção da esturua de metodos para atendimento a requisitos

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -4,11 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Biblioteca.Models
 {
-    public class EmprestimoService 
+    public class EmprestimoService
     {
         public void Inserir(Emprestimo e)
         {
-            using(BibliotecaContext bc = new BibliotecaContext())
+            using (BibliotecaContext bc = new BibliotecaContext())
             {
                 bc.Emprestimos.Add(e);
                 bc.SaveChanges();
@@ -17,7 +17,7 @@ namespace Biblioteca.Models
 
         public void Atualizar(Emprestimo e)
         {
-            using(BibliotecaContext bc = new BibliotecaContext())
+            using (BibliotecaContext bc = new BibliotecaContext())
             {
                 Emprestimo emprestimo = bc.Emprestimos.Find(e.Id);
                 emprestimo.NomeUsuario = e.NomeUsuario;
@@ -25,6 +25,7 @@ namespace Biblioteca.Models
                 emprestimo.LivroId = e.LivroId;
                 emprestimo.DataEmprestimo = e.DataEmprestimo;
                 emprestimo.DataDevolucao = e.DataDevolucao;
+                emprestimo.Devolvido = e.Devolvido;
 
                 bc.SaveChanges();
             }
@@ -32,18 +33,55 @@ namespace Biblioteca.Models
 
         public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
         {
-            using(BibliotecaContext bc = new BibliotecaContext())
+            using (BibliotecaContext bc = new BibliotecaContext())
             {
-                return bc.Emprestimos.Include(e => e.Livro).ToList();
+                IQueryable<Emprestimo> consulta;
+                if (filtro != null)
+                {
+                    switch (filtro.TipoFiltro)
+                    {
+                        case "Usuario":
+                            consulta = bc.Emprestimos.Where(e => e.NomeUsuario.Contains(filtro.Filtro));
+                            break;
+
+                        case "Livro":
+                            List<Livro> LivrosFiltrados = bc.Livros.Where(l => l.Titulo.Contains(filtro.Filtro)).ToList();
+
+                            List<int> LivrosIds = new List<int>();
+                            for (int i = 0; i < LivrosFiltrados.Count; i++)
+                            { LivrosIds.Add(LivrosFiltrados[i].Id); }
+
+                            consulta = bc.Emprestimos.Where(e => LivrosIds.Contains(e.LivroId));
+                            var debug = consulta.ToList();
+                            break;
+
+                        default:
+                            consulta = bc.Emprestimos;
+                            break;
+                    }
+                }
+                else
+                {
+                    consulta = bc.Emprestimos;
+                }
+                List<Emprestimo> ListaConsulta = consulta.OrderByDescending(e => e.DataDevolucao).ToList();
+                for (int i = 0; i < ListaConsulta.Count; i++)
+                {
+                    ListaConsulta[i].Livro = bc.Livros.Find(ListaConsulta[i].LivroId);
+
+                }
+                return ListaConsulta;
             }
+
         }
+    
 
         public Emprestimo ObterPorId(int id)
-        {
-            using(BibliotecaContext bc = new BibliotecaContext())
             {
+                using (BibliotecaContext bc = new BibliotecaContext())
+                {
                 return bc.Emprestimos.Find(id);
-            }
-        }
+                }
+             }
     }
 }
